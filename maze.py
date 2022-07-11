e16a48c72842628c3e266d2aa9de23b8513e914d
Mark Feeney <mark.feeney@gmail.com>
2022-07-11 23:32:49 +0000

    Added Kruskal's algorithm maze

--- maze.py
+++ maze.py
@@ -17,7 +17,7 @@ COL_OFFSET = {N: 0, E: 1, S: 0, W: -1}
 OPPOSITE = {N: S, E: W, S: N, W: E}
 
 # recursive backtracking
-def mazify(grid: List[List[int]], row: int, col: int) -> None:
+def mazify_rec(grid: List[List[int]], row: int, col: int) -> None:
     """Carve a maze into grid starting at (row, col)."""
     row_count = len(grid)
     col_count = len(grid[0])
@@ -34,7 +34,51 @@ def mazify(grid: List[List[int]], row: int, col: int) -> None:
            grid[next_row][next_col] == 0:
             grid[row][col] |= d
             grid[next_row][next_col] |= OPPOSITE[d]
-            mazify(grid, next_row, next_col)
+            mazify_rec(grid, next_row, next_col)
+
+def mazify_kruskal(grid: List[List[int]]) -> None:
+    row_count = len(grid)
+    col_count = len(grid[0])
+    # generate all our edges
+    edges = []
+    for row in range(row_count):
+        for col in range(col_count):
+            for d in [N, E, S, W]:
+                other_row = row + ROW_OFFSET[d]
+                other_col = col + COL_OFFSET[d]
+                if other_row >= 0 and other_row < row_count and \
+                   other_col >= 0 and other_col < col_count:
+                    edges.append((row, col, d))
+
+    # now we'll make an inline DSU/union-find for use in the actual algo
+    # We'll identify cells with id == row*col_count + col
+
+    # parent[i] points to the representative set for cell id i
+    parent = [i for i in range(row_count * col_count)] # cells start off disjoint
+    def find_set(cell_id: int) -> int:
+        if parent[cell_id] == cell_id:
+            return cell_id
+        # path compression
+        parent[cell_id] = find_set(parent[cell_id])
+        return parent[cell_id]
+
+    def union_set(set_a: int, set_b: int) -> None:
+        if set_a != set_b:
+            parent[set_b] = set_a
+        # should do union by size...
+
+    # randomize the edges and execute Kruskal's algo
+    random.shuffle(edges)
+    while edges:
+        row, col, d = edges.pop()
+        other_row = row + ROW_OFFSET[d]
+        other_col = col + COL_OFFSET[d]
+        set1 = find_set(row * col_count + col)
+        set2 = find_set(other_row * col_count + other_col)
+        if set1 != set2:
+            union_set(set1, set2)
+            grid[row][col] |= d
+            grid[other_row][other_col] |= OPPOSITE[d]
 
 def print_maze(grid: List[List[int]]) -> None:
     row_count = len(grid)
@@ -56,12 +100,12 @@ def print_maze(grid: List[List[int]]) -> None:
                 print("|", end="")
         print("")
 
-
 def main() -> None:
     size = 20
     # cell value of 0 means unvisited/"uncarved"
     grid = [[0] * size for _ in range(size)]
-    mazify(grid, 0, 0)
+    # mazify_rec(grid, 0, 0)
+    mazify_kruskal(grid)
     print_maze(grid)
 
 if __name__ == "__main__":
